a3ef21244dfaa54dd77096e8cabf778d342cf61d
Richard Fox <richard.fox@ahref.co.uk>
2013-01-19 03:06:18 +0000

    Forgot void on the deconstruction :(

--- GPSFilter.cpp
+++ GPSFilter.cpp
@@ -173,7 +173,7 @@ double GPSFilter::get_mph() {
   get_velocity(&delta_lat, &delta_lon);
   return calculate_mph(lat, lon, delta_lat, delta_lon);
 }
-GPSFilter::~GPSFilter(){
+void GPSFilter::~GPSFilter(){
   free_filter(f);
 }
 
